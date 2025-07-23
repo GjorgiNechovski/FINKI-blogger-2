96dadcd74b8c0cd8ba047f80208436cc71ca1594
GjorgiNechovski <gjorginechovski@gmail.com>
2025-07-23 22:09:45 +0200

    Replace styles with tailwind

--- Frontend/src/blogs/services/blog.service.ts
+++ Frontend/src/blogs/services/blog.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import { Observable, of, throwError } from 'rxjs'
+import { Observable } from 'rxjs'
+import { blogs_url, comments_url, headers, like_url } from '../../environment'
 import { Blog } from '../models/blog'
-import { mockBlogServiceResponses } from '../models/blog.mock'
 
 @Injectable({
   providedIn: 'root',
@@ -10,91 +10,55 @@ import { mockBlogServiceResponses } from '../models/blog.mock'
 export class BlogService {
   constructor(private httpClient: HttpClient) {}
 
-  // getBlogs(): Observable<Blog[]> {
-  //   return this.httpClient.get<Blog[]>(`${blogs_url}/blogs`)
-  // }
-
-  // createBlog(title: string, blogText: string): Observable<Blog> {
-  //   const data = { title: title, blog_text: blogText }
-  //   return this.httpClient.post<Blog>(`${blogs_url}/create`, data, {
-  //     headers,
-  //   })
-  // }
-
-  // getBlogDetails(blogId: string): Observable<Blog> {
-  //   return this.httpClient.get<Blog>(`${blogs_url}/blogs/${blogId}`, {
-  //     headers,
-  //   })
-  // }
-
-  // deleteBlog(blogId: number): Observable<void> {
-  //   return this.httpClient.post<void>(`${blogs_url}/deleteBlog/${blogId}`, {
-  //     headers,
-  //   })
-  // }
-
-  // createComment(blog_id: number, comment_text: string): Observable<void> {
-  //   const data = { blog_id: blog_id, comment_text: comment_text }
-  //   return this.httpClient.post<void>(`${comments_url}/create-comment`, data, {
-  //     headers,
-  //   })
-  // }
-
-  // deleteComment(comment_id: string): Observable<void> {
-  //   console.log(comment_id)
-
-  //   return this.httpClient.post<void>(
-  //     `${comments_url}/delete-comment?comment_id=${comment_id}`,
-  //     {},
-  //     {
-  //       headers,
-  //     },
-  //   )
-  // }
-
-  // likeBlog(blog_id: number) {
-  //   return this.httpClient.post<void>(
-  //     `${like_url}/like?blog_id=${blog_id}`,
-  //     {},
-  //     {
-  //       headers,
-  //     },
-  //   )
-  // }
-
   getBlogs(): Observable<Blog[]> {
-    return of(mockBlogServiceResponses.getBlogs())
+    return this.httpClient.get<Blog[]>(`${blogs_url}/blogs`)
   }
 
   createBlog(title: string, blogText: string): Observable<Blog> {
-    return of(mockBlogServiceResponses.createBlog(title, blogText))
+    const data = { title: title, blog_text: blogText }
+    return this.httpClient.post<Blog>(`${blogs_url}/create`, data, {
+      headers,
+    })
   }
 
   getBlogDetails(blogId: string): Observable<Blog> {
-    const blog = mockBlogServiceResponses.getBlogDetails(blogId)
-    if (blog) {
-      return of(blog)
-    }
-    return throwError(() => new Error(`Blog with ID ${blogId} not found`))
+    return this.httpClient.get<Blog>(`${blogs_url}/blogs/${blogId}`, {
+      headers,
+    })
   }
+
   deleteBlog(blogId: number): Observable<void> {
-    mockBlogServiceResponses.deleteBlog(blogId)
-    return of(void 0)
+    return this.httpClient.post<void>(`${blogs_url}/deleteBlog/${blogId}`, {
+      headers,
+    })
   }
 
   createComment(blog_id: number, comment_text: string): Observable<void> {
-    mockBlogServiceResponses.createComment(blog_id, comment_text)
-    return of(void 0)
+    const data = { blog_id: blog_id, comment_text: comment_text }
+    return this.httpClient.post<void>(`${comments_url}/create-comment`, data, {
+      headers,
+    })
   }
 
   deleteComment(comment_id: string): Observable<void> {
     console.log(comment_id)
-    mockBlogServiceResponses.deleteComment(comment_id)
-    return of(void 0)
+
+    return this.httpClient.post<void>(
+      `${comments_url}/delete-comment?comment_id=${comment_id}`,
+      {},
+      {
+        headers,
+      },
+    )
   }
 
-  likeBlog(blog_id: number): Observable<void> {
-    mockBlogServiceResponses.likeBlog(blog_id)
-    return of(void 0)
+  likeBlog(blog_id: number) {
+    return this.httpClient.post<void>(
+      `${like_url}/like?blog_id=${blog_id}`,
+      {},
+      {
+        headers,
+      },
+    )
   }
 }
